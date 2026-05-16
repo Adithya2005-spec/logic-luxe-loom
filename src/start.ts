2f7423f442e96bb24e1cf734416d8c9364745243
Adithya2005-spec <24ug1bybs051@bmsit.in>
2026-05-16 09:48:30 +0530

    trial

--- src/start.ts
+++ src/start.ts
@@ -14,7 +14,7 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
       status: 500,
       headers: { "content-type": "text/html; charset=utf-8" },
     });
-  }
+    
 export const startInstance = createStart(() => ({
   requestMiddleware: [errorMiddleware],
 }));
